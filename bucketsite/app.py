5e203fbb013a7b022e950b8d1d52aa26da4bde6a
monkut <shane.cousins@gmail.com>
2019-08-13 18:24:14 +0900

    Initial commit of simple s3 bucket static site serve

---
+++ bucketsite/app.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import sys
+from http import HTTPStatus
+from mimetypes import MimeTypes
+
+import boto3
+from flask import Flask, abort, request, send_file
+from flask_basicauth import BasicAuth
+
+from .settings import (
+    BASIC_AUTH_PASSWORD,
+    BASIC_AUTH_USERNAME,
+    BOTO3_ENDPOINTS,
+    BUCKET_NAME,
+    BUCKET_SITE_PREFIX,
+)
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] (%(name)s) %(funcName)s: %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+app.config["BASIC_AUTH_USERNAME"] = BASIC_AUTH_USERNAME
+app.config["BASIC_AUTH_PASSWORD"] = BASIC_AUTH_PASSWORD
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+logger.warning(app.config["BASIC_AUTH_USERNAME"])
+logger.warning(app.config["BASIC_AUTH_PASSWORD"])
+app.config["BASIC_AUTH_FORCE"] = True
+if not app.config["BASIC_AUTH_PASSWORD"] or not app.config["BASIC_AUTH_USERNAME"]:
+    raise ValueError(
+        f"Required environment variable not set: BASIC_AUTH_PASSWORD or BASIC_AUTH_USERNAME"
+    )
+basic_auth = BasicAuth(app)
+
+S3 = boto3.client("s3", endpoint_url=BOTO3_ENDPOINTS["s3"])
+
+
+@app.route("/<path:path>")
+@basic_auth.required
+def serve(path):
+    """Serve collection html in defined SITE_DIRECTORY_RELPATH"""
+    logger.debug(f"headers: {request.headers}")
+    if os.path.isabs(path) or ".." in path:
+        abort(HTTPStatus.NOT_FOUND)
+
+    key = f"{BUCKET_SITE_PREFIX}{path}"
+    try:
+        response = S3.get_object(Bucket=BUCKET_NAME, Key=key)
+    except S3.exceptions.NoSuchKey:
+        logger.warning(f"Key({key}) not found in: {BUCKET_NAME}")
+        abort(HTTPStatus.NOT_FOUND)
+
+    if path and "Body" in response:
+        mimer = MimeTypes()
+        file_mimetype = mimer.guess_type(path)[0]
+        request_file = response["Body"]
+        return send_file(request_file, add_etags=False, mimetype=file_mimetype)
+    abort(HTTPStatus.NOT_FOUND)
